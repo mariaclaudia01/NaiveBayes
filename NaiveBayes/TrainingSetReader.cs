68bdbda7e8c08b48b47205fca73d05e461d71dce
mihaistancu <stancu_meehigh@yahoo.com>
2016-05-05 00:32:05 +0300

    Removed unused "using" statements

--- NaiveBayes/TrainingSetReader.cs
+++ NaiveBayes/TrainingSetReader.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Xml.Linq;
 using System.Xml.XPath;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 
 namespace NaiveBayes
