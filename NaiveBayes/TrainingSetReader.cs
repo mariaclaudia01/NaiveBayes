a79b5252c619b5e9e72c3d6f870f730b180177ae
mihaistancu <stancu_meehigh@yahoo.com>
2016-05-05 00:49:43 +0300

    Fix possible issue with IEnumerable multiple enumeration

--- NaiveBayes/TrainingSetReader.cs
+++ NaiveBayes/TrainingSetReader.cs
@@ -12,16 +12,16 @@ namespace NaiveBayes
         {
             var wordXmlTags = ReadWordXmlTags(filename);
             int trainingItemsCount = (int)(wordXmlTags.Count() * 0.7);
-            return wordXmlTags
-                .Select(Parse)
+
+            return wordXmlTags.Select(Parse)
                 .Where(wordPartOfSpeech => IsValid(wordPartOfSpeech.Word))
                 .Take(trainingItemsCount);
         }
 
-        private IEnumerable<XElement> ReadWordXmlTags(string filename)
+        private List<XElement> ReadWordXmlTags(string filename)
         {
             return XDocument.Load(filename)
-               .XPathSelectElements("/text/sentence/word");
+               .XPathSelectElements("/text/sentence/word").ToList();
         }
 
         private WordPartOfSpeech Parse(XElement wordXmlTag)
@@ -32,6 +32,7 @@ namespace NaiveBayes
                 PartOfSpeech = wordXmlTag.Attribute("postag").Value[0].ToString().ToLower()
             };
         }
+
         private bool IsValid(string word)
         {
             return Regex.IsMatch(word, @"^[a-z_]+$");
