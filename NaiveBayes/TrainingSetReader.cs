ddd3f4728290214cfc2808de0abbeb5e408b4f89
mariaclaudia01 <marya_klau@yahoo.com>
2016-05-12 23:58:12 +0300

    Mapped postags from training.xml to a custom set of postags

--- NaiveBayes/TrainingSetReader.cs
+++ NaiveBayes/TrainingSetReader.cs
@@ -31,8 +31,30 @@ namespace NaiveBayes
             return new WordPartOfSpeech
             {
                 Word = wordXmlTag.Attribute("lemma").Value.ToLower(),
-                PartOfSpeech = wordXmlTag.Attribute("postag").Value[0].ToString().ToLower()
+                PartOfSpeech = Normalize(wordXmlTag.Attribute("postag").Value.ToLower())
             };
+        }
+
+        Dictionary<char, string> map = new Dictionary<char, string>
+        {
+            	{'t', "articol"},
+                {'n', "subst"},
+                {'a', "adj"},
+                {'v', "verb"},
+                {'q', "conj"},
+                {'p', "pron"},
+                {'r', "adv"},
+                {'c', "conj"},
+                {'d', "pron"},
+                {'i', "interj"}, 
+                {'m', "numeral"},
+        };
+
+        string Normalize(string posTag)
+        {
+            return map.ContainsKey(posTag[0])
+                ? map[posTag[0]]:
+                "altele";
         }
 
         private bool IsValid(WordPartOfSpeech trainingItem)
