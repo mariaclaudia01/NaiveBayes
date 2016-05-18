3e3125fb9f3440eb70125aacf4e4baa74d6c02c0
mihaistancu <stancu_meehigh@yahoo.com>
2016-05-19 01:03:48 +0300

    Refactoring

--- NaiveBayes/ListPercentageExtensions.cs
+++ NaiveBayes/ListPercentageExtensions.cs
@@ -7,15 +7,15 @@ namespace NaiveBayes
     {
         public static List<T> SkipPercent<T>(this List<T> sequence, double percentage)
         {
-            return sequence.Skip(GetItemCount(sequence, percentage)).ToList();
+            return sequence.Skip(sequence.Count(percentage)).ToList();
         }
 
         public static List<T> TakePercent<T>(this List<T> sequence, double percentage)
         {
-            return sequence.Take(GetItemCount(sequence, percentage)).ToList();
+            return sequence.Take(sequence.Count(percentage)).ToList();
         }
 
-        private static int GetItemCount<T>(List<T> sequence, double percentage)
+        private static int Count<T>(this List<T> sequence, double percentage)
         {
             return (int)(sequence.Count() * percentage);
         }
